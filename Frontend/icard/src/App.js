70ef8aff5b8b1ed9f215dc8c2498b3e827000366
sebastiancortina <sebastiancortina97@gmail.com>
2022-06-23 17:34:38 -0500

    react

---
+++ Frontend/icard/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <button className='btn btn-primary'>click</button>
+    </div>
+  );
+}
+
+export default App;
